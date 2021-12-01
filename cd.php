fcd3caba0606d7d0b8417f892e21b9bbc29d4e31
tastytim <tastytimgm@gmail.com>
2021-12-01 15:26:06 +0100

    milestone php commit

---
+++ cd.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+$cdList = [
+
+    [
+        "album" => "New Jersey",
+        "artist" => "Bon Jovi",
+        "year" => "1988",
+        "img" => "https://img.discogs.com/wNmFsb-w5m3ZG3ODfae1bkEUFkw=/fit-in/600x599/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-900670-1576304852-1235.jpeg.jpg"
+    ],
+    [
+        "album" => "Live at Wembley 86",
+        "artist" => "Queen",
+        "year" => "1992",
+        "img" => "https://m.media-amazon.com/images/I/71g40mlbinL._AC_SX450_.jpg"
+    ],
+    [
+        "album" => "Ten's Summoners Tales",
+        "artist" => "Sting",
+        "year" => "1993",
+        "img" => "https://www.evoluzionehifi.it/amplificatori/image/cache/data/STING%20TEN-400x400.jpg"
+    ],
+    [
+        "album" => "Steve Gadd Band",
+        "artist" => "Steve Gadd Band",
+        "year" => "2018",
+        "img" => "https://1.bp.blogspot.com/-3dMYgAJTyf0/Wr5bniQNcnI/AAAAAAABNE4/oZe0jT6Dqr0LaUI0ZalUTv9assWdu7imACLcBGAs/w1200-h630-p-k-no-nu/cover.jpg"
+    ],
+    [
+        "album" => "Brave New World",
+        "artist" => "Iron Maiden",
+        "year" => "2000",
+        "img" => "https://www.punkt.ch/repoimg/x2858-prod_main-Huxley.jpg.jpg.pagespeed.ic.oor3NxCBXT.jpg"
+    ],
+    [
+        "album" => "One More Car",
+        "artist" => "Eric Clapton",
+        "year" => "2002",
+        "img" => "https://img.discogs.com/5GLGwIjsAkUr3uiyDI5VZttZGRE=/fit-in/600x593/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1072468-1352438161-7671.jpeg.jpg"
+    ],
+    [
+        "album" => "Made In Japan",
+        "artist" => "Deep Purple",
+        "year" => "1972",
+        "img" => "https://upload.wikimedia.org/wikipedia/en/5/53/Made_in_Japan_%28Live_at_Parco_Capello%29.jpg"
+    ],
+    [
+        "album" => "And Justice For All",
+        "artist" => "Metallica",
+        "year" => "1988",
+        "img" => "https://th.static-thomann.de/thumb/orig/pics/bdb/128185/15106333_800.jpg"
+    ],
+    [
+        "album" => "Hard Wired",
+        "artist" => "Dave Weckl",
+        "year" => "1994",
+        "img" => "https://metalitalia.com/wp-content/uploads/2016/08/metallica-hardwired-hi-res-2016.jpeg"
+    ],
+    [
+        "album" => "Bad",
+        "artist" => "Michael Jackson ",
+        "year" => "1987",
+        "img" => "https://img.discogs.com/KH_lXUtclX6k6cLgFDIcvihtnps=/fit-in/600x968/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-418901-1526976153-5270.jpeg.jpg"
+    ],
+    
+]
+
+
+
+?>
---
+++ milestone1/index.php
@@ -0,0 +1,49 @@
+<?php
+   
+   include_once __DIR__ ."/../cd.php";
+   include __DIR__ ."/components/Card.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dischi</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../css/styles.css">
+
+
+</head>
+
+<body>
+    <header>
+
+        <div class="navbar-custom"></div>
+
+    </header>
+    <main>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row row-cols-5">
+
+                    <?php 
+            foreach($cdList as $cd){
+                printCard($cd);
+            }
+            ?>
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <footer></footer>
+</body>
+
+</html>
